616906d2491142cbcdbdf2c7891cf8c7ba549e1a
Dionei Silva <dionei.silva@gmail.com>
2022-12-11 12:56:27 -0300

    Atividade-Pratica-08-Funcoes

---
+++ script.js
@@ -0,0 +1,3 @@
+/* 1. Crie uma função que recebe 3 notas de um aluno por parâmetro e
+uma letra. Se a letra for A a função deve calcular a média aritmética,
+se for P, a média ponderada (peso: 5, 3 e 2). */
